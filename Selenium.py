0b25324e633818f874a7f48dca908655a542b01c
TheShadowAge <99925684+TheShadowAge@users.noreply.github.com>
2022-05-13 19:16:50 -0500

    Creating function for multiple boards
    I'm creating a function that can solve any number of boards

--- Selenium.py
+++ Selenium.py
@@ -93,8 +93,165 @@ def check_errada(element):
     texto = element.get_attribute('style')
     return 'normal' in texto
 
+def solucionar_multiplos(shadow, driver, dicio, input_field, how_many):
+    lista = []
+    for i in range(how_many):
+        lista.append(deepcopy(dicio))
+
+    cont = -1
+
+    wrong_checker = shadow.find_element('div[aria-live="assertive"]')
+
+    informacao = [[] for i in range(how_many)]
+    for i in range(how_many):
+        aux = [[] for i in range(4)]
+        aux[2] = [[] for i in range(5)]
+        aux[3] = [[] for i in range(5)]
+        informacao[i] = deepcopy(aux)
+    certo = [False for i in range(how_many)]
+    check = [False for i in range(how_many)]
+    while sum([len(lista[i]) for i in range(how_many)]) != how_many:
+        cont += 1
+        time.sleep(2)
+        row_board = []
+        board = 0
+        for i in range(how_many):
+            element = shadow.find_element('wc-board[id="board' + str(i) + '"]')
+            row = [shadow.find_element(element, 'div[lid="0"][termo-row="%i"]' % cont), shadow.find_element(element,'div[lid="1"][termo-row="%i"]' % cont), shadow.find_element(element,'div[lid="2"][termo-row="%i"]' % cont), shadow.find_element(element,'div[lid="3"][termo-row="%i"]' % cont), shadow.find_element(element,'div[lid="4"][termo-row="%i"]' % cont)]
+            row_board.append(row)
+
+        atuais = []
+
+        for i in row_board:
+            info = []
+            for j in range(5):
+                aux = i[j].get_attribute('aria-label')
+                if aux != '':
+                    info.append(verificar_letra(aux))
+            atuais.append(info)
+        
+        print(atuais)
+        
+        for i in range(how_many):
+            lista[i], informacao[i] = pesquisa(lista[i], atuais[i], informacao[i])
+        
+        print(informacao)
+
+        ter = []
+
+        lenghtt = []
+        for i in range(how_many):
+            lenghtt.append(len(lista[i]))
+
+        indice = lenghtt.index(max(lenghtt))
+
+
+        for j in informacao[indice][0]:
+            ter.append(j)
+        for j in informacao[indice][1]:
+            ter.append(j)
+
+        quant_letras = 5
+        palavras = ['']
+        sair = False
+
+        while len(palavras) != 0 and not(sair):
+            passou = 0
+            passou_ter = 0
+            while (len(palavras) == 0 or palavras == ['']) and not(any(certo)):
+                count_all = {'a': 0, 'b':0, 'c':0, 'd':0, 'e':0, 'f':0, 'g':0, 'h':0, 'i':0, 'j':0, 'k':0, 'l':0, 'm':0, 'n':0, 'o':0, 'p':0, 'q':0, 'r':0, 's':0, 't':0, 'u':0, 'v':0, 'w':0, 'x':0, 'y':0, 'z':0}
+
+                count = []
+                for i in range(how_many):
+                    count.append(count_letters(lista[i], ter))
+                
+                for i in range(how_many):
+                    for j in count[i]:
+                        count_all[j] += count[i][j]
+                
+                letters_count = []
+                for i in count_all.keys():
+                    letters_count.append([i, count_all[i]])
+                
+                letters_count.sort(key=lambda x: x[1], reverse=True)
+
+                aux_inf = [[] for i in range(4)]
+                aux_inf[2] = [[] for i in range(5)]
+                aux_inf[3] = [[] for i in range(5)]
+
+                letras = letters_count[:5]
+
+                letras_selecionadas = [i[0] for i in letras]
+                letters = []
+                letters.append(letras_selecionadas.pop(0))
+                aleatorio = random.sample(letras_selecionadas, quant_letras - 1)
+                letters.extend(aleatorio)
+
+                aux_inf[0] = letters
+
+                palavras, aux = pesquisa(dicio, [], aux_inf)
+
+                if len(palavras) == 0 and quant_letras > 3:
+                    quant_letras -= 1
+                elif quant_letras == 3 and passou < 5:
+                    quant_letras = 4
+                    passou += 1
+                elif passou_ter < 1:
+                    ter = []
+                
+                    for j in informacao[indice][0]:
+                        ter.append(j)
+                    passou = 0
+                    passou_ter += 1
+                else:
+                    passou_ter = 0
+                    quant_letras = 2
+
+            denovo = True
+            while denovo and not(any(certo)) and len(palavras) != 0:
+                palavra = palavras.pop(palavras.index(random.choice(palavras)))
+                word_without_accents = ""
+                for letter in palavra:
+                    word_without_accents += accented_letters[letter]
+                input_field.send_keys(word_without_accents)
+                input_field.send_keys(Keys.ENTER)
+                if check_errada(wrong_checker):
+                    denovo = True
+                    input_field.send_keys(Keys.BACKSPACE)
+                    input_field.send_keys(Keys.BACKSPACE)
+                    input_field.send_keys(Keys.BACKSPACE)
+                    input_field.send_keys(Keys.BACKSPACE)
+                    input_field.send_keys(Keys.BACKSPACE)
+                else:
+                    denovo = False
+                    sair = True
+            for i in range(how_many):
+                print(len(lista[i]))
+            for i in range(how_many):
+                if len(lista[i]) == 1 and check[i] == False:
+                    certo[i] = True
+            print(certo)
+            if any(certo):
+                    sair = True
+        length = [len(lista[i]) for i in range(how_many)]
+        time.sleep(1)
+        while True in certo:
+            for i in range(how_many):
+                time.sleep(1)
+                if certo[i] == True:
+                    word_without_accents = ''
+
+                    for letter in lista[i][0]:
+                        word_without_accents += accented_letters[letter]
+                    input_field.send_keys(word_without_accents)
+                    input_field.send_keys(Keys.ENTER)
+
+                    certo[i] = False
+                    check[i] = True
+            cont += 1
+
 def solucionar(shadow, dicio, input_field):
-    lista = dicio
+    lista = deepcopy(dicio)
     cont = 0
 
     wrong_checker = shadow.find_element('div[aria-live="assertive"]')
@@ -102,10 +259,13 @@ def solucionar(shadow, dicio, input_field):
     informacao = [[] for i in range(4)]
     informacao[2] = [[] for i in range(5)]
     informacao[3] = [[] for i in range(5)]
-
+    
 
 
     while len(lista) != 1:
+
+        time.sleep(1)
+
         row = [shadow.find_element('div[lid="0"][termo-row="%i"]' % cont), shadow.find_element('div[lid="1"][termo-row="%i"]' % cont), shadow.find_element('div[lid="2"][termo-row="%i"]' % cont), shadow.find_element('div[lid="3"][termo-row="%i"]' % cont), shadow.find_element('div[lid="4"][termo-row="%i"]' % cont)]
 
         atuais = []
@@ -163,11 +323,11 @@ def solucionar(shadow, dicio, input_field):
                 else:
                     denovo = False
                     sair = True
-                time.sleep(1)
+                time.sleep(0.2)
             if len(lista) == 1:
                 sair = True
             cont += 1
-            time.sleep(1)
+            time.sleep(0.2)
 
     word_without_accents = ''
 
@@ -177,12 +337,11 @@ def solucionar(shadow, dicio, input_field):
     input_field.send_keys(word_without_accents)
     input_field.send_keys(Keys.ENTER)
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
+PATH = os.path.join(dirname, "chromedriver")
 
 chromeOptions = Options()
-chromeOptions.add_argument("--kiosk")
+chromeOptions.add_argument("--remote-debugging-port=9222")
 driver = webdriver.Chrome(PATH, chrome_options=chromeOptions)
-
 shadow = Shadow(driver)
 
 driver.get("https://term.ooo/")
@@ -205,7 +364,47 @@ element.send_keys(Keys.ENTER)
 
 time.sleep(2)
 
-solucionar(shadow, dicio, element)
+solucionar_multiplos(shadow, driver, dicio, element, 1)
+
+time.sleep(3)
+
+duo = shadow.find_element('a[id="duo"]')
+
+duo.click()
+
+time.sleep(1)
+
+elem = driver.find_element_by_tag_name("html")
+
+ac.move_to_element(elem).click().perform()
+
+element = driver.find_element_by_tag_name("body")
+
+element.send_keys("serao")
+
+element.send_keys(Keys.ENTER)
+
+solucionar_multiplos(shadow, driver, dicio, element, 2)
+
+time.sleep(3)
+
+quarteto = shadow.find_element('a[id="quatro"]')
+
+quarteto.click()
+
+time.sleep(1)
+
+element = driver.find_element_by_tag_name("body")
+
+elem = driver.find_element_by_tag_name("html")
+
+ac.move_to_element(elem).click().perform()
+
+element.send_keys("serao")
+
+element.send_keys(Keys.ENTER)
+
+solucionar_multiplos(shadow, driver, dicio, element, 4)
 
 time.sleep(5)
 
